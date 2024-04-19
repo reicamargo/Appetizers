e9bb922a40f7ad704c7ca4a6e482da8bdcaed88b
Reinaldo Camargo <camargo.reinaldo@hotmail.com>
2024-04-19 15:18:03 -0300

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Reinaldo Camargo on 19/04/24.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
